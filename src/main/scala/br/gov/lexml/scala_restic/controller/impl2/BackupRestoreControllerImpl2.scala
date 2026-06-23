603be1dc937c60767f18500ed5b0c48c083268bb
João R. M. Nicola <joaoraf@gmail.com>
2026-06-23 15:43:49 -0300

    WIP

--- src/main/scala/br/gov/lexml/scala_restic/controller/impl2/BackupRestoreControllerImpl2.scala
+++ src/main/scala/br/gov/lexml/scala_restic/controller/impl2/BackupRestoreControllerImpl2.scala
@@ -4,7 +4,8 @@ import zio.*
 import zio.stream.*
 import zio.config.*
 import zio.config.magnolia.*
-import BackupRestoreControllerImpl2.{Message,State}
+import BackupRestoreControllerImpl2.{Message, State}
+import br.gov.lexml.scala_restic.command.ResticCommandService
 
 final case class BackupRestoreControllerImplConfig(
 )
@@ -17,7 +18,8 @@ class BackupRestoreControllerImpl2 private (
   private val config : BackupRestoreControllerImplConfig,
   private val queue : Queue[BackupRestoreControllerImpl2.Message],
   private val eventLoopFiberPromise : Promise[Nothing,Fiber[Nothing,Unit]],
-  private val stateRef : SubscriptionRef[State]
+  private val stateRef : SubscriptionRef[State],
+  private val resticCommandService : ResticCommandService
 ):
   private def start = for {
     eventLoopFiber <- eventLoop.forkScoped
@@ -28,29 +30,52 @@ class BackupRestoreControllerImpl2 private (
   import State.*
   import Message.*
 
+  final case class Process(task : Task[Option[(State,Option[Process])]])
+
   private def eventLoop : UIO[Unit] = {
-    def loop : UIO[Unit] = for {
-      currentState <- stateRef.get
-      nextMessage <- queue.take
-      nextStateAndProcess <- processStateMessage(currentState,nextMessage)
-      (nextState,process) = nextStateAndProcess
-      _ <- stateRef.set(nextState)
-      _ <- process.foldCauseZIO(
+    def executeProcess(process : Process) : UIO[Unit] =
+      process.task.foldCauseZIO(
         cause => stateRef.set(Stopped(cause)),
         {
           case None => stateRef.set(Stopped())
-          case Some(nextState2) => stateRef.set(nextState2) *> loop
-        }
-      )
+          case Some((nextState2,optProcess)) =>
+            stateRef.set(nextState2) *>
+            optProcess.fold(loop)(executeProcess)
+        })
+    def loop : UIO[Unit] = for {
+      currentState <- stateRef.get
+      nextMessage <- queue.take
+      process = processStateMessage(currentState,nextMessage)
+      _ <- executeProcess(process)
     } yield ()
     loop
   }
 
-  private def processStateMessage(currentState : State, message : Message)
-  : UIO[(State,Task[Option[State]])] =
+  private def startProcess : Task[Option[(State,Option[Process])]] =
+      // Check if the restic repo path exists.
+      // If not, create it if the initIfNecessary flag is set, otherwise throw an exception.
+      // If the repo path exists, check if it's a valid restic repo using the cat info command.
+      // If not, throw an exception.
+      // If the repo path exists and is a valid restic repo and the initialRestore is set, return the
+      // Restoring state and the restore process.
+      // Otherwise, call the schedule task.
+    ZIO.ifZIO(ZIO.attemptBlockingIO(config.repo.repoPath.toFile.exists))(
+      onFalse =
+        if config.initIfNecessary then
+          resticCommandService.init(repo,password = config.password))(
+            ZIO.unit
+            ,
+            ZIO.unit
+          )
+        else ZIO.fail(new Exception("Repository does not exist!")),
+      onTrue =
+        if config.restoreOnStart then
+          ZIO.succeed()
+    )
+
+  private def processStateMessage(currentState : State, message : Message) : Process =
     (currentState,message) match {
-      case (Stopped(_),Start) =>
-        ZIO.succeed((Starting,doStart))
+      case (Stopped(_),Start) => Process(startProcess)
     }
 
   private def doStart : Task[Option[State]] = ???
