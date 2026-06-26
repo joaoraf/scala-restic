a6cee51be631e5a98f106fbfae97dd78e9f37555
João R. M. Nicola <joaoraf@gmail.com>
2026-06-25 22:26:50 -0300

    WIP

--- src/main/scala/br/gov/lexml/scala_restic/controller/impl2/BackupRestoreControllerImpl2.scala
+++ src/main/scala/br/gov/lexml/scala_restic/controller/impl2/BackupRestoreControllerImpl2.scala
@@ -6,21 +6,34 @@ import zio.config.*
 import zio.config.magnolia.*
 import BackupRestoreControllerImpl2.{Message, State}
 import br.gov.lexml.scala_restic.command.ResticCommandService
+import br.gov.lexml.scala_restic.controller.BackupRestoreController.State.WAITING_COMMAND
+import br.gov.lexml.scala_restic.controller.impl.BackupRestoreControllerConfig
+import br.gov.lexml.scala_restic.data.restore.RestoreMessage
+import br.gov.lexml.scala_restic.options.common.Repo
+import br.gov.lexml.scala_restic.options.snapshots.SnapshotsOptions
+import cron4s.*
+import cron4s.lib.javatime.*
 
-final case class BackupRestoreControllerImplConfig(
-)
+import java.nio.file.Files
+import java.time.OffsetDateTime
+
 
 enum BackupRestoreControllerImplMessage:
   case BackupNowAsync(reply : Promise[Throwable,Unit])
   case BackupNowSync(reply : Promise[Throwable,Unit])
 
 class BackupRestoreControllerImpl2 private (
-  private val config : BackupRestoreControllerImplConfig,
+  private val config: BackupRestoreControllerConfig,,
   private val queue : Queue[BackupRestoreControllerImpl2.Message],
   private val eventLoopFiberPromise : Promise[Nothing,Fiber[Nothing,Unit]],
   private val stateRef : SubscriptionRef[State],
+  private val backupScheduleFiberRef : Ref[Option[Fiber[Nothing,Unit]]],
   private val resticCommandService : ResticCommandService
 ):
+
+  private val repo = Repo.atFolder(config.resticRepo.repoPath)
+  private val password = Option(config.resticRepo.repoPassword).filterNot(_.isBlank)
+
   private def start = for {
     eventLoopFiber <- eventLoop.forkScoped
     _ <- eventLoopFiberPromise.succeed(eventLoopFiber)
@@ -30,10 +43,10 @@ class BackupRestoreControllerImpl2 private (
   import State.*
   import Message.*
 
-  final case class Process(task : Task[Option[(State,Option[Process])]])
+  final case class ProcessStep(task : Task[Option[(State,Option[ProcessStep])]])
 
   private def eventLoop : UIO[Unit] = {
-    def executeProcess(process : Process) : UIO[Unit] =
+    def executeProcess(process : ProcessStep) : UIO[Unit] =
       process.task.foldCauseZIO(
         cause => stateRef.set(Stopped(cause)),
         {
@@ -51,39 +64,95 @@ class BackupRestoreControllerImpl2 private (
     loop
   }
 
-  private def startProcess : Task[Option[(State,Option[Process])]] =
-      // Check if the restic repo path exists.
-      // If not, create it if the initIfNecessary flag is set, otherwise throw an exception.
-      // If the repo path exists, check if it's a valid restic repo using the cat info command.
-      // If not, throw an exception.
-      // If the repo path exists and is a valid restic repo and the initialRestore is set, return the
-      // Restoring state and the restore process.
-      // Otherwise, call the schedule task.
-    ZIO.ifZIO(ZIO.attemptBlockingIO(config.repo.repoPath.toFile.exists))(
-      onFalse =
-        if config.initIfNecessary then
-          resticCommandService.init(repo,password = config.password))(
-            ZIO.unit
-            ,
-            ZIO.unit
-          )
-        else ZIO.fail(new Exception("Repository does not exist!")),
-      onTrue =
-        if config.restoreOnStart then
-          ZIO.succeed()
-    )
-
-  private def processStateMessage(currentState : State, message : Message) : Process =
-    (currentState,message) match {
-      case (Stopped(_),Start) => Process(startProcess)
+  private def processStateMessage(currentState: State, message: Message): ProcessStep =
+    (currentState, message) match {
+      case (Stopped(_), Start) => ProcessStep(startProcess)
     }
 
-  private def doStart : Task[Option[State]] = ???
+  private type StepTask = Task[Option[(State,Option[ProcessStep])]]
+
+  private def step(state : State, task : StepTask) =
+    ZIO.succeed(Some((state,Some(ProcessStep(task)))))
+
+  private def step(state: State) =
+    ZIO.succeed(Some((state, None)))
+    
+  private def stepOut = ZIO.succeed(None)  
+
+  private val toScheduleStep : StepTask = step(Starting,scheduleTask)
+
+  private val toRestoreStep : StepTask =
+    ZIO.clockWith(_.currentDateTime).flatMap { now => step(Restoring(now),restoreTask) }
+
+  private def fail(message : String) =  ZIO.fail(Exception(message))
+
+  private def startProcess : StepTask =
+    // Check if the restic repo path exists.
+    def checkRepositoryExistence : StepTask =
+      ZIO.ifZIO(ZIO.attemptBlockingIO(Files.exists(config.resticRepo.repoPath)))(whenPathExists, whenPathDoesNotExist)
+    // If not, create it if the initIfNecessary flag is set, otherwise throw an exception.
+    def whenPathDoesNotExist : StepTask =
+      if(config.initIfNecessary) then
+        resticCommandService.init(repo,password = password) *> toScheduleStep
+      else
+        fail("Restic backup folder does not exists and initialization is not enabled.")
+
+    // If the repo path exists, check if it's a valid restic repo using the cat info command.
+    // If not, throw an exception.
+    def whenPathExists : StepTask =
+      ZIO.ifZIO(resticCommandService.checkRepositoryExistence(repo,password = password))(
+        checkIfShouldRestore
+        , fail("Restic backup folder exists but is not a valid restic repository.")
+      )
+    def checkIfShouldRestore : StepTask =
+      for {
+        snapshots <- resticCommandService.snapshots(
+          repo,
+          password = password,
+          snapshotsOptions = SnapshotsOptions(latest = 1)
+        )
+        anyBackupFolderExists <- ZIO.exists(config.backupFolders)(folder =>
+          ZIO.attempt(Files.exists(config.basePath.resolve(folder).normalize))
+        )
+        result <- snapshots.snapshots.headOption match {
+          case Some(snapshot) if config.restoreIfEmpty && !anyBackupFolderExists => toRestoreStep
+          case _ => toScheduleStep
+        }
+      } yield result
+    checkRepositoryExistence
+  end startProcess
+
+  private def scheduleTask : StepTask =
+    Clock.currentDateTime.flatMap { now =>
+      config.backupCronSchedule.next(now) match
+        case Some(nextRun) =>
+          val delay =
+            if nextRun.toInstant.isAfter(now.toInstant) then
+              Duration.fromInterval(now.toInstant, nextRun.toInstant)
+            else Duration.Zero
+          for {
+            _ <- backupScheduleFiberRef.get.flatMap {
+              case Some(fiber) => fiber.interrupt
+              case None => ZIO.unit
+            }
+            newBackupScheduleFiber <- (Clock.sleep(delay) *> queue.offer(ScheduledBackup)).unit.fork
+            _ <- backupScheduleFiberRef.set(Some(newBackupScheduleFiber))
+          } yield ()
+        case None => ZIO.unit
+    }.map(_ => Some((Ready,None)))
+
+  private def restoreTask : StepTask = ???
+
+  
+
 
 object BackupRestoreControllerImpl2:
   private enum Message:
     case Start
+    case ScheduledBackup
 
   enum State:
     case Stopped(cause: Cause[Any] = Cause.empty) extends State
     case Starting
+    case Ready
+    case Restoring(startTime : OffsetDateTime, latestSummary: Option[RestoreMessage.Summary] = None) extends State
